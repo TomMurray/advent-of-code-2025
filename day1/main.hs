8874e008ece839ba652c1c0c2009e35aa8ccc747
Tom Murray <tom.paul.murray@gmail.com>
2025-12-02 18:29:57 +0000

    Complete day 1 part 2

--- day1/main.hs
+++ day1/main.hs
@@ -4,8 +4,23 @@ dir :: Num a => String -> a
 dir "L" = -1
 dir "R" = 1
 
-easywrap :: Int -> Int
-easywrap a = ((a `mod` 100) + 100) `mod` 100
+enumToInt :: Bool -> Int
+enumToInt False = 0
+enumToInt True  = 1
+
+-- We maintain a range of -99 -> 99
+-- We first add the rotation and see if we ticked through zero
+count :: Int -> Int -> (Int, Int)
+count n r =
+    let n' = n + r
+        crossed = abs (signum n' - signum n) == 2
+        exact = n' == 0
+        wrapcount = abs n' `div` 100
+        totalcount = wrapcount + enumToInt crossed + enumToInt exact
+    in (totalcount, n' - wrapcount * 100 * signum n')
+
+wrap :: Int -> Int
+wrap a = ((a `mod` 100) + 100) `mod` 100
 
 main :: IO ()
 main = do
@@ -15,7 +30,11 @@ main = do
 
     -- Start at 50
     let start = 50
-    let cumPositions = scanl (\acc r -> easywrap (acc + r)) start rotations
-    
-    -- Count the number of zeroes
-    print (length (filter (== 0) cumPositions))
+
+    -- Part 1
+    let positions = scanl (\acc r -> wrap (acc + r)) start rotations
+    print (length (filter (== 0) positions))
+
+    -- Part 2
+    let zerocounts = map fst $ scanl (\(_, n) r -> count n r) (0, start) rotations
+    print (sum zerocounts)
