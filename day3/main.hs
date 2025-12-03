5098da7b4fd9c16382762f389c4011d4677f3d08
Tom Murray <tom.paul.murray@gmail.com>
2025-12-03 08:44:35 +0000

    Add working day 3 part 1

---
+++ day3/main.hs
@@ -0,0 +1,26 @@
+import Data.Char (digitToInt)
+
+-- argmax which also returns the value
+argmax :: (Num b, Ord a) => [a] -> (a, b)
+argmax (x:xs) =
+  case xs of
+    [] -> (x, 0)
+    _ ->
+      let (maxs, idxs) = argmax xs in
+      -- N.B. >= to ensure we get the earliest instance of the max value
+      if x >= maxs then (x, 0) else (maxs, idxs + 1)
+
+maxJoltage _ 0 = 0
+maxJoltage l digits =
+  let (val, idx) = argmax (take (length l - digits + 1) l)
+  in val * 10 ^ (digits - 1) + maxJoltage (drop (idx + 1) l) (digits - 1)
+main = do
+  text <- readFile "input.txt"
+  let banks = map (map digitToInt) (lines text)
+
+  let maxJoltages = map (flip maxJoltage 2) banks
+
+  putStrLn $ "Part 1: " ++ show (sum maxJoltages)
+
+
+
