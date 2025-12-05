3d8c7e476c6f1e09916e3a5dec18eca6220cd77a
Tom Murray <tom.paul.murray@gmail.com>
2025-12-05 23:18:04 +0000

    Add working day 5 part 1 implementation

--- day5/main.hs
+++ day5/main.hs
@@ -1,5 +1,4 @@
 module Main where
-import Distribution.Simple.Utils (xargs)
 
 splitOnce :: Eq a => a -> [a] -> ([a], [a])
 splitOnce delimiter l =
@@ -16,6 +15,7 @@ parseInterval s =
 
 -- Like a binary search & split, specifically for
 -- intervals, splitting before the interval that contains x
+splitBefore [] x = ([], [])
 splitBefore [(begin, end)] x
   | x <= end + 1 = ([], [(begin, end)])
   | otherwise = ([(begin, end)],[])
@@ -23,29 +23,30 @@ splitBefore [(begin, end)] x
 -- Result is 2 lists of intervals
 splitBefore iset x =
   let (left, right) = splitAt (length iset `div` 2) iset
-      lhsEnd = if null left then minBound else snd (last left)
-      (ll, lr) = if x <= lhsEnd + 1 then splitBefore left x else (left, [])
-      (rl, rr) = if x <= lhsEnd + 1 then (right, []) else splitBefore right x
-  in if x <= lhsEnd + 1 then (ll, lr ++ rl) else (ll ++ rl, rr)
+      splitLeft = (not (null left) && (x <= snd (last left) + 1))
+      (ll, lr) = if splitLeft then splitBefore left x else (left, [])
+      (rl, rr) = if splitLeft then (right, []) else splitBefore right x
+  in if splitLeft then (ll, lr ++ rl) else (ll ++ rl, rr)
 
+splitAfter [] x = ([], [])
 splitAfter [(begin, end)] x
   | x >= begin - 1 = ([(begin, end)], [])
   | otherwise = ([], [(begin, end)])
 
 splitAfter iset x =
   let (left, right) = splitAt (length iset `div` 2) iset
-      rhsBegin = if null right then maxBound else fst (head right)
-      (ll, lr) = if x >= rhsBegin - 1 then (left, []) else splitAfter left x
-      (rl, rr) = if x >= rhsBegin - 1 then splitAfter right x else (right, [])
-  in if x >= rhsBegin - 1 then (ll ++ rl, rr) else (ll, lr ++ rl)
+      splitRight = (not (null right) && (x >= fst (head right) - 1))
+      (ll, lr) = if splitRight then (left, []) else splitAfter left x
+      (rl, rr) = if splitRight then splitAfter right x else (right, [])
+  in if splitRight then (ll ++ rl, rr) else (ll, lr ++ rl)
 
 -- Build an interval set by insertion
+writeInterval [] i = [i]
 writeInterval iset (begin, end) =
   let (b0, e0) = head iset
       (bn, en) = last iset
   in [(min begin b0, max end en)]
 
-insertInterval :: (Num a, Bounded a, Ord a) => [(a, a)] -> (a, a) -> [(a, a)]
 -- Base case: empty set becomes the intervalinsertInterval [] i = [i]
 insertInterval [] i = [i]
 insertInterval iset (begin, end) =
@@ -60,17 +61,18 @@ insertInterval iset (begin, end) =
 
 inInterval :: Ord a => (a, a) -> a -> Bool
 inInterval (begin, end) x = begin <= x && x <= end
+
 contains [] _ = False
 contains [i] x = inInterval i x
 contains iset x =
   let (left, right) = splitAt (length iset `div` 2) iset
-      rhsBegin = if null right then maxBound else fst (head right)
-    in if x < rhsBegin then contains left x else contains right x
+      searchRight = (not (null right) && (x >= fst (head right)))
+    in if searchRight then contains right x else contains left x
 
 
 main :: IO ()
 main = do
-  text <- readFile "example.txt"
+  text <- readFile "input.txt"
   let (fresh, available) = splitOnce "" $ lines text
 
   -- Parse intervals
@@ -79,6 +81,10 @@ main = do
   print freshIntervals
   print availableItems
 
-  -- TODO: Construct an interval set
-  --       Basically binary search insertion
-  --       Also binary search lookup
+
+  -- Construct an interval set of the intervals
+  let intervalSet = foldl insertInterval [] freshIntervals
+
+  -- Part 1
+  let availableAndFresh = length $ filter (contains intervalSet) availableItems
+  print availableAndFresh
