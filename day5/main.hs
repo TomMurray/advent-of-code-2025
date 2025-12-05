6de96d505c2bd233a735c4b0530eaaee713819ea
Tom Murray <tom.paul.murray@gmail.com>
2025-12-05 09:58:23 +0000

    Add input parsing for day 5, leave it there

--- day5/main.hs
+++ day5/main.hs
@@ -1,6 +1,24 @@
 module Main where
 
+splitOnce :: Eq a => a -> [a] -> ([a], [a])
+splitOnce delimiter l =
+  let (before, after) = break (== delimiter) l
+  in (before, tail after)
+
+type Interval = (Integer, Integer)
+
+parseInterval :: String -> Interval
+parseInterval s =
+  let (begin, end) = break (== '-') s
+  in (read begin, read $ tail end)
+
 main :: IO ()
 main = do
   text <- readFile "input.txt"
-  print $ lines text
+  let (fresh, available) = splitOnce "" $ lines text
+    
+  -- Parse intervals
+  let freshIntervals = map parseInterval fresh
+  let availableItems = map read available :: [Integer]
+  print freshIntervals
+  print availableItems
