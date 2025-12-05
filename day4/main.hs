50190cb2b8df3683248d186006d0f16708bbaa36
Tom Murray <tom.paul.murray@gmail.com>
2025-12-05 09:17:32 +0000

    Bit of refactoring

--- day4/main.hs
+++ day4/main.hs
@@ -15,14 +15,11 @@ pad1D :: Num a => Int -> [a] -> [a]
 pad1D n l =
   replicate n 0 ++ l ++ replicate n 0
 
-pad2D :: Num a => Int -> [[a]] -> [[a]]
-pad2D n l =
-  let width = length (head l)
-      height = length l
-      outerPadding = replicate n $ replicate (width + n * 2) 0
-  in outerPadding ++ map (pad1D n) l ++ outerPadding
-
+conv1D :: Num b => Int -> [b] -> [b]
 conv1D n = map sum . windows n . pad1D (n `div` 2)
+
+boolToInt :: Num a => Bool -> a
+boolToInt x = if x then 1 else 0
 
 conv2D :: Num a => Int -> [[a]] -> [[a]]
 conv2D n l =
@@ -31,18 +28,31 @@ conv2D n l =
       rowPadding = replicate padAmount $ replicate width 0
   in map (foldl (zipWith (+)) (replicate width 0)) $ windows 3 $ map (conv1D n) $ rowPadding ++ l ++ rowPadding
 
+findRemovable m = do
+  let surroundCount = conv2D 3 m
+  let masked = zipWith (zipWith (*)) surroundCount m
+  let removableRolls = map (map (boolToInt . liftM2 (&&) (> 0) (<= 4))) masked
+  removableRolls
+
+popcount :: [[Int]] -> Int
+popcount m =
+  sum $ map (length . filter (== 1)) m
+
 main :: IO ()
 main = do
-  text <- readFile "input.txt"
+  text <- readFile "example.txt"
 
   -- Map input lines to [[Int]]
   let rolls = map (map mapInputChar) $ lines text
 
-  let surroundCount = conv2D 3 rolls 
-  let masked = zipWith (zipWith (*)) surroundCount rolls
+  let removableRolls = findRemovable rolls
+  let count = popcount removableRolls
 
-  let removableRolls = map (map (liftM2 (&&) (> 0) (<= 4))) masked
-  let count = sum $ map (length . filter id) removableRolls
+  -- Part 1
+  putStrLn $ "Part 1: " ++ show count
 
-  -- Now work out which rolls are surrounded by < 4 other rolls via sum pooling
-  print count
+  -- Part 2
+  -- Iterate until a fixed point (no more rolls can be removed) and
+  -- give the total number of rolls removed.
+
+  
