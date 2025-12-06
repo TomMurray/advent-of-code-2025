d16bc099b6726e2292d6064aa8678203f5b312a6
Tom Murray <tom.paul.murray@gmail.com>
2025-12-06 09:49:09 +0000

    Add day 6 part 1 solution

--- day6/main.hs
+++ day6/main.hs
@@ -7,11 +7,22 @@ transpose lists
   | length (head lists) == 1 = [join lists]
   | otherwise = map head lists:transpose (map tail lists)
 
+getFoldOp "+" = (+)
+getFoldOp "*" = (*)
+
+compute problem =
+  let foldOp = getFoldOp (last problem)
+  in foldr1 foldOp (map read $ init problem)
+
 main :: IO ()
 main = do
   text <- readFile "input.txt"
 
   let grid = map words $ lines text
   let problems = transpose grid
-  
-  print $ problems
+
+  -- Compute
+  let solutions = map compute problems
+
+  -- Part 1
+  putStrLn $ "Part 1: " ++ show (sum solutions)
