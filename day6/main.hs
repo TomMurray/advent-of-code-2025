ad0b8c7218a9c65ee21d18a58016c6fe03fb84a6
Tom Murray <tom.paul.murray@gmail.com>
2025-12-06 10:22:21 +0000

    Working day 6 part 2 solution

--- day6/main.hs
+++ day6/main.hs
@@ -7,12 +7,25 @@ transpose lists
   | length (head lists) == 1 = [join lists]
   | otherwise = map head lists:transpose (map tail lists)
 
+padTo :: Int -> [Char] -> [Char]
+padTo digits str =
+  str ++ replicate (digits - length str) ' '
+
 getFoldOp "+" = (+)
 getFoldOp "*" = (*)
 
 compute problem =
   let foldOp = getFoldOp (last problem)
   in foldr1 foldOp (map read $ init problem)
+
+computePart2 problem =
+  let foldOp = getFoldOp [last (head problem)]
+  in foldr1 foldOp $ map (read . init) problem
+
+splitBy f [] = []
+splitBy f (x:xs) =
+  let (left, right) = break f xs
+  in (x:left) : if null right then [] else splitBy f (tail right)
 
 main :: IO ()
 main = do
@@ -26,3 +39,10 @@ main = do
 
   -- Part 1
   putStrLn $ "Part 1: " ++ show (sum solutions)
+
+  -- Part 2
+  let transposed = transpose $ lines text
+  -- Split by empty columns
+  let problems = splitBy (all (== ' ')) transposed
+  let solutions = map computePart2 problems
+  putStrLn $ "Part 2: " ++ show (sum solutions)
