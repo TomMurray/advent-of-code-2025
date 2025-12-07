3c7fb6c8f619723fa55afd438ef26b4bba17ff46
Tom Murray <tom.paul.murray@gmail.com>
2025-12-07 10:10:20 +0000

    Day 7 part 2 working

--- day7/main.hs
+++ day7/main.hs
@@ -1,30 +1,39 @@
 module Main where
 
-import Data.List (tails)
+import Data.List (tails, elemIndex)
 
 windows :: Int -> [a] -> [[a]]
 windows n = map (take n) . takeWhile ((>=n) . length) . tails
 
-pad1D :: Int -> [Bool] -> [Bool]
+pad1D :: Num a => Int -> [a] -> [a]
 pad1D n l =
-  replicate n False ++ l ++ replicate n False
+  replicate n 0 ++ l ++ replicate n 0
 
-conv1D :: [Bool] -> [Bool] -> [Bool]
 conv1D kernel =
   let kernelWidth = length kernel
-  in map (or . zipWith (&&) kernel) . windows kernelWidth . pad1D (kernelWidth `div` 2)
+  in map (sum . zipWith (*) kernel) .windows kernelWidth . pad1D (kernelWidth `div` 2)
 
+isSplitter '^' = 1
+isSplitter _ = 0
 
+isStart 'S' = 1
+isStart _ = 0
 
-isSplitter c = c == '^'
-
-computeRow (count, beams) row =
-  let intersections = zipWith (&&) beams (map isSplitter row)
-      splitBeams = conv1D [True, False, True] intersections
-      splitCount = length $ filter id intersections
-      maskedBeams = zipWith (&&) beams (map not intersections)
-      nextBeams = zipWith (||) maskedBeams splitBeams
+part1Row (count, beams) row =
+  let intersections = zipWith (*) beams (map isSplitter row)
+      splitCount = sum intersections
+      maskedBeams = zipWith (*) beams (map (1-) intersections)
+      splitBeams = map (min 1) $ conv1D [1, 0, 1] intersections
+      nextBeams = map (min 1) $ zipWith (+) maskedBeams splitBeams
   in (count + splitCount, nextBeams)
+
+part2Row beams row =
+  let intersected = zipWith (*) beams (map isSplitter row)
+      maskedBeams = zipWith (*) beams (map ((1-) . min 1) intersected)
+      splitBeams = conv1D [1, 0, 1] intersected
+      nextBeams = zipWith (+) maskedBeams splitBeams
+  in nextBeams
+
 
 main :: IO ()
 main = do
@@ -34,15 +43,20 @@ main = do
   -- State for the algorithm is a count (the answer)
   -- and the current row of active beams (True or False).
   -- I'm going to use a dense list for simplicity.
-  
+
   -- Assume the first row must have an 'S' in it
-  let beams = map (== 'S') (head rows)
+  let beams = map isStart (head rows)
 
   -- One iteration checks each position in the current beams list
   -- with splitters in the next row in the grid:
   --  * First compute beam/splitter intersections
   --  * Then update beams list by removing the beam at intersected
   --    positions and setting beams in neighbouring positions.
-  let result = foldl computeRow (0, beams) (tail rows)
+  let result = foldl part1Row (0, beams) (tail rows)
   putStrLn $ "Part 1: " ++ show (fst result)
-  
+
+  -- Part 2 many worlds
+  -- I think this is similar but without limiting to 1 beam per position
+  -- and instead summing the number of beams in each position.
+  let result = sum $ foldl part2Row beams (tail rows)
+  putStrLn $ "Part 2: " ++ show result
